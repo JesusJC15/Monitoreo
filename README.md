6ed45756aec13bffbb1b17e9158974517dce4247
JesusJC15 <jesus.jauregui-c@mail.escuelaing.edu.co>
2025-02-24 10:26:25 -0500

    Creación del repositorio

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+# PARCIAL PRIMER CORTE - SOLID, PATRONES, TDD, SPRING
+## Jesús Alberto Jauregui Conde
 
+### SpringBoot
+Se uso Spring Initializr para la inicialización del proyecto
+https://start.spring.io/
+
+![](/src/main/resources/images/1.png)
---
+++ src/main/java/edu/eci/cvds/Monitoreo/MonitoreoApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.Monitoreo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoreoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoreoApplication.class, args);
+	}
+
+}
